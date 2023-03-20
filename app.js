9cfa9a09e1e6f8096703c6b9d62d522a8e99e9c1
MufidAlnadi <mufidhussein@gmail.com>
2023-03-20 20:38:00 +0300

    finish

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // Q1
-console.log(-9*3)
-console.log("value is: "+50 )
-console.log(17%5)
-console.log(5%17)
-console.log(5/10)
-console.log(4===4)
-console.log(4!=5)
-console.log(7<=8)
+console.log(-9 * 3)
+console.log("value is: " + 50)
+console.log(17 % 5)
+console.log(5 % 17)
+console.log(5 / 10)
+console.log(4 === 4)
+console.log(4 != 5)
+console.log(7 <= 8)
 let x = 10;
-console.log(Math.ceil(x)-Math.floor(x))
+console.log(Math.ceil(x) - Math.floor(x))
 
 
 
@@ -22,12 +22,12 @@ alert(num)
 
 let num1 = prompt("Enter the first number: ")
 let num2 = prompt("Enter the second number:")
-if(num1 > num2){
-    alert( num1 +" "+ num2)
+if (num1 > num2) {
+  alert(num1 + " " + num2)
 }
-else if (num2 > num1){
+else if (num2 > num1) {
 
-}    alert( num2 +" "+ num1)
+} alert(num2 + " " + num1)
 
 
 
@@ -35,12 +35,12 @@ else if (num2 > num1){
 
 let num_1 = prompt("Enter the first number: ")
 let num_2 = prompt("Enter the second number:")
-if(num_1 > num2){
-    alert( num_1 )
+if (num_1 > num2) {
+  alert(num_1)
 }
-else if (num_2 > num_1){
+else if (num_2 > num_1) {
 
-}    alert( num_2)
+} alert(num_2)
 
 
 
@@ -54,43 +54,43 @@ alert("The sum is: " + sum);
 
 
 
-switch(num) {
-    case num == 1:
-      alert("ONE")
-      break;
-    case num == 2:
-      alert("TWO")
-      break;
-    case num == 3:
-      alert("THREE")
-      break;
-    case num == 4:
-      alert("FOUR")
-      break;
-    case num == 5:
-      alert("FIVE")
-      break;
-    case num == 6:
-      alert("SIX")
-      break;
-    case num == 7:
-      alert("SEVEN")
-      break;
-    case num == 8:
-      alert("EIGHT")
-      break;
-  
-    default:
-        alert("NINE")
-    }
+switch (num) {
+  case num == 1:
+    alert("ONE")
+    break;
+  case num == 2:
+    alert("TWO")
+    break;
+  case num == 3:
+    alert("THREE")
+    break;
+  case num == 4:
+    alert("FOUR")
+    break;
+  case num == 5:
+    alert("FIVE")
+    break;
+  case num == 6:
+    alert("SIX")
+    break;
+  case num == 7:
+    alert("SEVEN")
+    break;
+  case num == 8:
+    alert("EIGHT")
+    break;
+
+  default:
+    alert("NINE")
+}
 
 
 for (let i = 0; i <= 5; i++) {
-    alert(i)
-  }
+  alert(i)
+}
 
 
-let message = ""; 
+let message = "";
 // add the number to the string with a space to display it in one message
 for (let i = 0; i <= 5; i++) {
   message += i + " ";
@@ -98,11 +98,36 @@ for (let i = 0; i <= 5; i++) {
 alert(message);
 
 
-for ( let i =0 ; i <=20 ;i++){
-   console.log(i%3)
+for (let i = 0; i <= 20; i++) {
+  console.log(i % 3)
 }
 
-let i=0;
-while(i <= 100){
-
+let rang = prompt('number in the range 0…100.');
+while (rang >= 100 || rang <= 0) {
+  rang = prompt("prompt again until a valid number is entered")
 }
+let str = prompt('Enter Somthing:"');
+while (isNaN(str) == false) {
+    str = prompt("false prompt again")
+}
+
+
+let sumOfTheNumber =  prompt('Enter a number')
+
+let result=0;
+for (let i =0 ; i <= sumOfTheNumber ; i++){
+    result = result+i
+    
+}
+alert(result)
+
+let AvgOfTheNumber =  prompt('Enter a number')
+let arr=[];
+let AvgResult=0;
+for (let i =0 ; i <= avgOfTheNumber ; i++){
+    result = result+i
+    for (let j=0 ; j <= avgOfTheNumber;j++){
+        arr[j] = result
+    }
+}
+alert(AvgResult/arr.length)
